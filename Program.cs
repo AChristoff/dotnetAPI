617b384a9078f51f44443c6aa3f9a8f3e93f233f
Alex Hristov <aleksandar.hristov@cchellenic.com>
2024-10-29 12:25:28 +0200

    \

--- Program.cs
+++ Program.cs
@@ -10,16 +10,38 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Dev_CORS", corsBuilder =>
+        {
+            corsBuilder.WithOrigins("http://localhost:3000") // React dev server
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+
+
+    options.AddPolicy("Prod_CORS", corsBuilder =>
+        {
+            corsBuilder.WithOrigins("https://costschef.com")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("Dev_CORS");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else
 {
+    app.UseCors("Prod_CORS");
     app.UseHttpsRedirection();
 }
 
